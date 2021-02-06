8361d2244f7d6b4aa4c2f5e5c502eb8b0bdaca40
EldasDEV | Shavkat Toshpulotov <73429646+eldasdev@users.noreply.github.com>
2021-02-06 21:13:39 +0500

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+/*
+  Developrer: Shavkat Toshpulotov | EldasDEV
+  GitHub: https://github.com/eldasdev
+  Portfolio Site: https://eldasdev.netlify.app
+  LICENSE: MIT Open Source Projects Protect LICENSE
+  Telegram Chat: https://t.me/eldasdev
+
+  // Please read the readme.md file in the GitHub Repository \\
+
+*/
